1f31b330de59948f3d7f2307223227e54e23075c
amoskared <107379748+amoskared@users.noreply.github.com>
2022-08-05 09:49:45 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -20,6 +20,12 @@ const isNumber = require('is-number');
 var router = express.Router()
 
 
+//―――――――――――――――――――――――――――――――――――――――――― ┏  LIB  ┓ ―――――――――――――――――――――――――――――――――――――――――― \\
+const { ytPlay, ytMp3, ytMp4 } = require("../lib/youtube");
+const { GDrive } = require("../lib/scrape/gdrive");
+const { mediafire2 } = require("../lib/scrape/mediafire");
+const zippy = require("../lib/scrape/zippy");
+
 //―――――――――――――――――――――――――――――――――――――――――― ┏  Info  ┓ ―――――――――――――――――――――――――――――――――――――――――― \\
 //  >>>>>Menu<<<<<
 // >Dowloader
@@ -40,6 +46,191 @@ var router = express.Router()
 //―――――――――――――――――――――――――――――――――――――――――― ┏  Dowloader  ┓ ―――――――――――――――――――――――――――――――――――――――――― \\
 
 
+router.get('/dowloader/youtubemp3', async (req, res, next) => {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+	if (!url.includes('youtu')) return res.json(loghandler.noturl)
+
+	let { yta, ytv } = require('../lib/y2mate')
+	let mp3 = await yta(url, '320kbps')
+	let mp4 = await ytv(url, '360p')
+	if (!mp4.title ) return res.json(loghandler.noturl)
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: {
+				Title: mp4.title,
+				thumb : mp4.thumb,
+				Idvideo: mp4.id,
+				mp3: mp3.dl_link,
+
+	} })
+})
+router.get('/dowloader/youtubemp4', async (req, res, next) => {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+	if (!url.includes('youtu')) return res.json(loghandler.noturl)
+
+	let { yta, ytv } = require('../lib/y2mate')
+	let mp3 = await yta(url, '320kbps')
+	let mp4 = await ytv(url, '360p')
+	if (!mp4.title ) return res.json(loghandler.noturl)
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: {
+				Title: mp4.title,
+				thumb : mp4.thumb,
+				Idvideo: mp4.id,
+				mp4: mp4.dl_link
+
+	} })
+})
+router.get('/dowloader/youtubemp3v2', async (req, res, next) => {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+	if (!url.includes('youtu')) return res.json(loghandler.noturl)
+
+	ytMp3(url)
+	.then(data => {
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: `${data}`
+		})
+		})
+})
+router.get('/dowloader/youtubemp4v2', async (req, res, next) => {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+	if (!url.includes('youtu')) return res.json(loghandler.noturl)
+
+	ytMp4(url)
+	.then(data => {
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: `${data}`
+		})
+		})
+})
+router.get('/downloader/youtubeplay', async (req, res, next) => {
+	var text1 = req.query.text;
+	var apikey = req.query.apikey
+	if (!text1 ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter text"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+
+	let yts = require("yt-search")
+	let search = await yts(text1)
+	let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
+
+	let { yta, ytv } = require('../lib/y2mate')
+	let mp3 = await yta(anu.url, '320kbps')
+	let mp4 = await ytv(anu.url, '360p')
+	
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: {
+				Link: anu.url,
+				Title: anu.title,
+				Description : anu.description,
+				Idvideo: anu.videoId,
+				Duration: anu.timestamp,
+				Viewer: anu.views,
+				UploadedOn : anu.ago,
+				Author : anu.author.name,
+				Channel : anu.author.url,
+				linldowloader: {
+					mp3: mp3.dl_link,
+					mp4:{ 
+						link: mp4.dl_link,
+						filesize: mp4.filesizeF
+					}
+				}
+
+	} })
+})
+router.get('/dowloader/youtubeplayv2', async (req, res, next) => {
+	var text1 = req.query.text;
+	var apikey = req.query.apikey
+	if (!text1 ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter text"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+
+	ytPlay(text1)
+	.then(data => {
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: `${data}`
+		})
+		})
+})
+router.get('/dowloader/gdrive', async (req, res, next) => {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+	if (!url.includes('drive.google')) return res.json(loghandler.noturl)
+
+	GDrive(url)
+	.then(data => {
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: `${data}`
+		})
+		})
+})
+router.get('/dowloader/mediafire', async (req, res, next) => {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+	if (!url.includes('mediafire.com')) return res.json(loghandler.noturl)
+
+	mediafire2(url)
+	.then(data => {
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: `${data}`
+		})
+		})
+})
+router.get('/dowloader/zippyshare', async (req, res, next) => {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
+	if (!url.includes('zippyshare.com')) return res.json(loghandler.noturl)
+
+	zippy(url)
+	.then(data => {
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: `${data}`
+		})
+		})
+})
 router.get('/dowloader/fbdown', async (req, res, next) => {
 	var url = req.query.url;
 	var apikey = req.query.apikey
@@ -172,31 +363,6 @@ router.get('/dowloader/igdowloader', async (req, res, next) => {
 			 res.json(loghandler.error)
 })
 
-})
-
-router.get('/dowloader/yt', async (req, res, next) => {
-	var url = req.query.url;
-	var apikey = req.query.apikey
-	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
-	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
-	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
-
-	let { yta, ytv } = require('../lib/y2mate')
-	let mp3 = await yta(url, '320kbps')
-	let mp4 = await ytv(url, '360p')
-	if (!mp4.title ) return res.json(loghandler.noturl)
-		res.json({
-			status: true,
-			creator: `${creator}`,
-			result: {
-				Title: mp4.title,
-				thumb : mp4.thumb,
-				filesize: mp4.filesizeF,
-				Idvideo: mp4.id,
-				mp3: mp3.dl_link,
-				mp4: mp4.dl_link
-
-	} })
 })
 
 router.get('/dowloader/soundcloud', async (req, res, next) => {
@@ -1064,44 +1230,6 @@ function logResults(error, results) {
 }
 })
 
-
-router.get('/search/ytplay', async (req, res, next) => {
-	var text1 = req.query.text;
-	var apikey = req.query.apikey
-	if (!text1 ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter text"})
-	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
-	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey) 
-
-	let yts = require("yt-search")
-	let search = await yts(text1)
-	let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
-
-	let { yta, ytv } = require('../lib/y2mate')
-	let mp3 = await yta(anu.url, '320kbps')
-	let mp4 = await ytv(anu.url, '360p')
-		res.json({
-			status: true,
-			creator: `${creator}`,
-			result: {
-				Link: anu.url,
-				Title: anu.title,
-				Description : anu.description,
-				Idvideo: anu.videoId,
-				Duration: anu.timestamp,
-				Viewer: anu.views,
-				UploadedOn : anu.ago,
-				Author : anu.author.name,
-				Channel : anu.author.url,
-				linldowloader: {
-					mp3: mp3.dl_link,
-					mp4:{ 
-						link: mp4.dl_link,
-						filesize: mp4.filesizeF
-					}
-				}
-
-	} })
-})
 
 router.get('/search/sticker', async (req, res, next) => {
 	var text1 = req.query.text;
