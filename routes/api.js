c0ace7cb292cfdd5797f55b02494c897087af2af
amoskared <107379748+amoskared@users.noreply.github.com>
2022-08-10 15:57:42 +0000

    apitrash

--- routes/api.js
+++ routes/api.js
@@ -31,6 +31,7 @@ const {fetchText, fetchJson, runtime, getBuffer, readTxt, readJson } = require('
 const apis = require("../lib/listdl")
 
 const { mp31, mp32, mp41, mp42, play1, play2, gdrivedl, zippydl, mediafiredl, fbdl, twitterdl, tiktokdl, igdl, scdl, igstorydl, teledl } = require('./downloader')
+const { meganebuk_, kusobyquery, kusobyurl, anoboys_, anoboydl_ } = require('./animanga')
 const { nh_info, nh_search, nh_read, nh_pdf, dd_info, dd_latest, dd_search, dd_read, dd_pdf } = require('./nsfw')
 
 async function sleep(ms) {
@@ -89,6 +90,15 @@ router.get('/downloader/telesticker', teledl);
 
 //―――――――――――――――――――――――――――――――――――――――――― ┏  ANIME - MANGA  ┓ ―――――――――――――――――――――――――――――――――――――――――― \\
 
+router.get('/animanga/meganebuk', meganebuk_);
+
+router.get('/animanga/kusonime-by-query', kusobyquery);
+
+router.get('/animanga/kusonime-by-url', kusobyurl);
+
+router.get('/animanga/anoboysearch', anoboys_);
+
+router.get('/animanga/anoboydl', anoboydl_);
 
 //―――――――――――――――――――――――――――――――――――――――――― ┏  NSFW  ┓ ―――――――――――――――――――――――――――――――――――――――――― \\
 
