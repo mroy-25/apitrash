894be4ee48af1497914be332dbe36b6a486bf059
amoskared <107379748+amoskared@users.noreply.github.com>
2022-08-15 16:50:39 +0700

    apitrash

--- routes/nsfw.js
+++ routes/nsfw.js
@@ -9,6 +9,7 @@ const request = require("request");
 const {fetchText, fetchJson, runtime, getBuffer, readTxt, readJson } = require('../lib/myfunc')
 const { DDlatest, DDdownload, DDsearch } = require("../lib/scrape/doudesu");
 
+
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -22,13 +23,17 @@ async function nh_info(req, res, next) {
 	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
 	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
 	
+	try {
 	let nh = await fetchJson(`https://janda.mod.land/nhentai/get?book=${code}`)
 	
 	res.json({
 			status: true,
 	        	creator: `${creator}`,
 			result: nh.data
-		})
+		})	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
 }
 async function nh_search(req, res, next) {
 	var key = req.query.key;
@@ -41,13 +46,17 @@ async function nh_search(req, res, next) {
 	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
 	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
 	
+	try {
 	let nh = await fetchJson(`https://janda.mod.land/nhentai/search/?key=${key}&sort=${sort}&page=${page}`)
 	
 	res.json({
 			status: true,
 	        	creator: `${creator}`,
 			result: nh.data
-		})
+		})	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
 }
 async function nh_read(req, res, next) {
 	var code = req.query.code;
@@ -56,7 +65,7 @@ async function nh_read(req, res, next) {
 	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
 	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
 	
-	
+	try {
 	let data = await axios.get(`https://trash-apis.herokuapp.com/api/nsfw/nhentai-info?code=${code}&apikey=${apikey}`)
 	let result = data.data.result
 	let restjson = result.image
@@ -89,7 +98,10 @@ async function nh_read(req, res, next) {
 	</head>
 	<body>`
 	for(let url of duckJson) html += `<img src=${url}>`
-		res.send(html)
+		res.send(html)	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
 }
 async function nh_pdf(req, res, next) {
 	var code = req.query.code;
@@ -129,6 +141,137 @@ async function nh_pdf(req, res, next) {
 	await res.sendFile(__path + `/tmp/nhentai/${code}.pdf`)
     	await sleep(3000)
     	await fss.unlinkSync(__path + `/tmp/nhentai/${code}.pdf`)
+	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
+}
+//pururin
+async function prr_info(req, res, next) {
+	var code = req.query.code;
+	var apikey = req.query.apikey
+	if (!code ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter code"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	try {
+	let prr = await fetchJson(`https://janda.mod.land/pururin/get?book=${code}`)
+	
+	res.json({
+			status: true,
+	        	creator: `${creator}`,
+			result: prr.data
+		})	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
+}
+async function prr_search(req, res, next) {
+	var key = req.query.key;
+	var sort = req.query.sort;
+	var page = req.query.page;
+	var apikey = req.query.apikey
+	if (!key ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter key"})
+	if (!sort ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter sort"})
+	if (!page ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter page"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	try {
+	let prr = await fetchJson(`https://janda.mod.land/pururin/search/?key=${key}&sort=${sort}&page=${page}`)
+	
+	res.json({
+			status: true,
+	        	creator: `${creator}`,
+			result: prr.data
+		})	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
+}
+async function prr_read(req, res, next) {
+	var code = req.query.code;
+	var apikey = req.query.apikey
+	if (!code ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter code"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	try {
+	let data = await axios.get(`https://trash-apis.herokuapp.com/api/nsfw/pururin-info?code=${code}&apikey=${apikey}`)
+	let result = data.data.result
+	let restjson = result.image
+	let title = result.title
+	let duckJson = await restjson.map(a => 'https://external-content.duckduckgo.com/iu/?u=' + a)
+	let html = `<!DOCTYPE html>
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>${title}</title>
+	<style>
+	img {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 100%;
+	}
+	body {
+		background-color: #1a202c;
+		background-color: rgba(26, 32, 44, 1);
+	}
+	@media (min-width: 576px) {
+		img {
+			width: auto;
+			max-width: 100%;
+			height: auto;
+		}
+	}
+	</style>
+	</head>
+	<body>`
+	for(let url of duckJson) html += `<img src=${url}>`
+		res.send(html)	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
+}
+async function prr_pdf(req, res, next) {
+	var code = req.query.code;
+	var apikey = req.query.apikey
+	if (!code ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter code"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	try {
+	let data = await axios.get(`https://trash-apis.herokuapp.com/api/nsfw/pururin-info?code=${code}&apikey=${apikey}`)
+	let result = data.data.result
+	let restjson = result.image
+	let array_page = await restjson.map(a => 'https://external-content.duckduckgo.com/iu/?u=' + a)
+	let count = 0;
+	let ResultPdf = [];
+	
+	for (let i = 0; i < array_page.length; i++) {
+		//if (!fs.existsSync("./tmp/nhentai")) fs.mkdirSync("./tmp/nhentai");
+		let image_name = "./tmp/pururin/" + code + i + ".jpg";
+		await new Promise((resolve) =>
+			request(array_page[i]).pipe(fss.createWriteStream(image_name)).on("finish", resolve)
+		);
+		console.log(array_page[i]);
+		ResultPdf.push(image_name);
+		count++;
+	}
+
+	await new Promise((resolve) =>
+		topdf(ResultPdf, "A4")
+			.pipe(fss.createWriteStream("./tmp/pururin/" + code + ".pdf"))
+			.on("finish", resolve)
+	);
+
+	for (let i = 0; i < array_page.length; i++) {
+		fss.unlink("./tmp/pururin/" + code + i + ".jpg");
+	}
+	await res.sendFile(__path + `/tmp/pururin/${code}.pdf`)
+    	await sleep(3000)
+    	await fss.unlinkSync(__path + `/tmp/pururin/${code}.pdf`)
 	
 } catch(err) {
        res.json({ error: err.message }) 
@@ -198,7 +341,7 @@ async function dd_read(req, res, next) {
 	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
 	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
 	
-	
+	try {
 	let data = await axios.get(`https://trash-apis.herokuapp.com/api/nsfw/doujindesu-info?url=${url}&apikey=${apikey}`)
 	let result = data.data.result
 	let restjson = result.image
@@ -231,7 +374,10 @@ async function dd_read(req, res, next) {
 	</head>
 	<body>`
 	for(let url of duckJson) html += `<img src=${url}>`
-		res.send(html)
+		res.send(html)	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
 }
 async function dd_pdf(req, res, next) {
 	var url = req.query.url;
@@ -278,4 +424,172 @@ async function dd_pdf(req, res, next) {
      }
 }
 
-module.exports = { nh_info, nh_search, nh_read, nh_pdf, dd_info, dd_latest, dd_search, dd_read, dd_pdf };
+// sekaikomik
+async function sk_info(req, res, next) {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	const { Download } = require("../lib/scrape/sekaikomik");
+	Download(url)
+	.then((data) =>{
+	res.json({
+			status: true,
+	        	creator: `${creator}`,
+			result: data
+		})
+	})
+	      .catch((err) =>{
+ res.json(loghandler.error)
+})
+}
+async function sk_search(req, res, next) {
+	var query = req.query.query;
+	var apikey = req.query.apikey
+	if (!query ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter query"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	const { Search } = require("../lib/scrape/sekaikomik");
+	Search(query)
+	.then((data) =>{
+	res.json({
+			status: true,
+	        	creator: `${creator}`,
+			result: data
+		})
+	})
+		.catch((err) =>{
+ res.json(loghandler.error)
+})
+}
+async function sk_latest(req, res, next) {
+	var apikey = req.query.apikey
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	const { Latest } = require("../lib/scrape/sekaikomik"); const { Download, Popular, Latest, Search } = require("../lib/scrape/sekaikomik");
+	Latest()
+	.then((data) =>{
+	res.json({
+			status: true,
+	        	creator: `${creator}`,
+			result: data
+		})
+	})
+		.catch((err) =>{
+ res.json(loghandler.error)
+})
+}
+
+async function sk_popular(req, res, next) {
+	var apikey = req.query.apikey
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	const { Popular } = require("../lib/scrape/sekaikomik");
+	Popular()
+	.then((data) =>{
+	res.json({
+			status: true,
+	        	creator: `${creator}`,
+			result: data
+		})
+	})
+		.catch((err) =>{
+ res.json(loghandler.error)
+})
+}
+
+async function sk_read(req, res, next) {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	try {
+	let data = await axios.get(`https://trash-apis.herokuapp.com/api/nsfw/sekaikomik-info?url=${url}&apikey=${apikey}`)
+	let result = data.data.result
+	let restjson = result.images
+	let title = result.title
+	let duckJson = await restjson.map(a => 'https://external-content.duckduckgo.com/iu/?u=' + a)
+	let html = `<!DOCTYPE html>
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>${title}</title>
+	<style>
+	img {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 100%;
+	}
+	body {
+		background-color: #1a202c;
+		background-color: rgba(26, 32, 44, 1);
+	}
+	@media (min-width: 576px) {
+		img {
+			width: auto;
+			max-width: 100%;
+			height: auto;
+		}
+	}
+	</style>
+	</head>
+	<body>`
+	for(let url of duckJson) html += `<img src=${url}>`
+		res.send(html)	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
+}
+async function sk_pdf(req, res, next) {
+	var url = req.query.url;
+	var apikey = req.query.apikey
+	if (!url ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter url"})
+	if (!apikey ) return res.json({ status : false, creator : `${creator}`, message : "[!] masukan parameter apikey"})
+	if (apikey != `${keyapi}`) return res.json(loghandler.notapikey)
+	
+	try {
+	let data = await axios.get(`https://trash-apis.herokuapp.com/api/nsfw/sekaikomik-info?url=${url}&apikey=${apikey}`)
+	let result = data.data.result
+	let restjson = result.images
+	let title = result.title
+	let array_page = await restjson.map(a => 'https://external-content.duckduckgo.com/iu/?u=' + a)
+	let count = 0;
+	let ResultPdf = [];
+	
+	for (let i = 0; i < array_page.length; i++) {
+		//if (!fs.existsSync("./tmp/nhentai")) fs.mkdirSync("./tmp/nhentai");
+		let image_name = "./tmp/sekaikomik/dd" + i + ".jpg";
+		await new Promise((resolve) =>
+			request(array_page[i]).pipe(fss.createWriteStream(image_name)).on("finish", resolve)
+		);
+		console.log(array_page[i]);
+		ResultPdf.push(image_name);
+		count++;
+	}
+
+	await new Promise((resolve) =>
+		topdf(ResultPdf, "A4")
+			.pipe(fss.createWriteStream("./tmp/sekaikomik/" + title + ".pdf"))
+			.on("finish", resolve)
+	);
+
+	for (let i = 0; i < array_page.length; i++) {
+		fss.unlink("./tmp/sekaikomik/dd" + i + ".jpg");
+	}
+	await res.sendFile(__path + `/tmp/sekaikomik/${title}.pdf`)
+    	await sleep(3000)
+    	await fss.unlinkSync(__path + `/tmp/sekaikomik/${title}.pdf`)
+	
+} catch(err) {
+       res.json({ error: err.message }) 
+     }
+}
+module.exports = { nh_info, nh_search, nh_read, nh_pdf, prr_info, prr_search, prr_read, prr_pdf, dd_info, dd_latest, dd_search, dd_read, dd_pdf, sk_latest, sk_popular, sk_search, sk_read, sk_pdf };
