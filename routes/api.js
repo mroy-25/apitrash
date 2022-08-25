b54da8f1d5d7ef4a8db8acce1d6fa126980f7940
amoskared <107379748+amoskared@users.noreply.github.com>
2022-08-25 15:01:06 +0700

    apitrash

--- routes/api.js
+++ routes/api.js
@@ -32,7 +32,7 @@ const apis = require("../lib/listdl")
 const samehadaku = require("../lib/samehadaku")
 
 const { mp31, mp32, mp41, mp42, play1, play2, gdrivedl, zippydl, mediafiredl, fbdl, twitterdl, tiktokdl, igdl, scdl, igstorydl, teledl } = require('./downloader')
-const { otaku_home, meganebuk_, kusobyquery, kusobyurl, anoboys_, anoboydl_ } = require('./animanga')
+const { meganebuk_, kusobyquery, kusobyurl, anoboys_, anoboydl_ } = require('./animanga')
 const { nh_info, nh_search, nh_read, nh_pdf, nh_zip, prr_info, prr_search, prr_read, prr_pdf, dd_info, dd_latest, dd_search, dd_read, dd_pdf, sk_info, sk_latest, sk_popular, sk_search, sk_read, sk_pdf } = require('./nsfw')
 const { otaku_home, otaku_genre, otaku_search, otaku_dl } = require('./otakudesu')
 
